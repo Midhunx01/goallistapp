2a2c0465247d21676f41a5f54b78d0b9145d6b55
Midhun Mohan <midhunxda.123@gmail.com>
2023-02-13 22:10:49 +0530

    Added Ripple Effect

--- components/goalList.js
+++ components/goalList.js
@@ -2,11 +2,13 @@ import { StyleSheet, Text, View, Pressable } from "react-native";
 
 function GoalList(props) {
     return (
-        <Pressable onPress={props.onDelete.bind(this, props.id)}>
+        
         <View style={styles.goalView}> 
+        <Pressable android_ripple={{color: '#210644'}} onPress={props.onDelete.bind(this, props.id)} style={({pressed}) => pressed && styles.pressedItem}>
             <Text style={styles.goalText}>{props.text}</Text> 
-        </View>
         </Pressable>
+        </View>
+        
     );
 }
 
@@ -16,11 +18,14 @@ export default GoalList;
 const styles= StyleSheet.create({
     goalView: {
         margin: 8,
-        padding: 8,
         borderRadius: 10,
         backgroundColor: '#5e0acc',
       },
       goalText: {
         color: 'white',
+        padding: 8,
+      },
+      pressedItem: {
+        opacity: 0.5,
       }
 })
